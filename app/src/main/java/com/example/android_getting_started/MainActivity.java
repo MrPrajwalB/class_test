3c57c8c1ca79da0dbe124fd8bd102cf9d6bc4272
Prajwal Bende <34344073+MrPrajwalB@users.noreply.github.com>
2019-08-30 11:19:42 +0530

    Add files via upload

--- app/src/main/java/com/example/android_getting_started/MainActivity.java
+++ app/src/main/java/com/example/android_getting_started/MainActivity.java
@@ -9,15 +9,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.TextView;
+import java.util.Random;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.series.DataPoint;
+import com.jjoe64.graphview.series.LineGraphSeries;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
     private static final String TAG = "MainActivity";
     private SensorManager sensorManager;
+    private LineGraphSeries<DataPoint> series;
     private Sensor mLight;
-    private Sensor accel;
-
-    private TextView light, x, y, z;
-
+    private TextView light;
+    private float lux;
+    private int lastX = 0;
 
     @Override
     public final void onCreate(Bundle savedInstanceState) {
@@ -25,16 +34,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         setContentView(R.layout.activity_main);
 
         light = (TextView) findViewById(R.id.textView);
-        x = (TextView) findViewById(R.id.textView2);
-        y = (TextView) findViewById(R.id.textView3);
-        z = (TextView) findViewById(R.id.textView4);
 
         sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
         mLight = sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
-        if (mLight == null){
-                Log.d(TAG, "Light Sensor is not present");
+        if (mLight == null) {
+            Log.d(TAG, "Light Sensor is not present");
         }
-        accel = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        GraphView graph = (GraphView) findViewById(R.id.graph);
+        // data
+        series = new LineGraphSeries<DataPoint>();
+        graph.addSeries(series);
+        // customize a little bit viewport
+        Viewport viewport = graph.getViewport();
+        viewport.setYAxisBoundsManual(true);
+        viewport.setXAxisBoundsManual(true);
+        viewport.setMinY(0);
+        viewport.setMaxY(100);
+        viewport.setMinX(0);
+        viewport.setMaxX(100);
+        viewport.setScrollable(true);
 
     }
 
@@ -47,29 +65,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     public final void onSensorChanged(final SensorEvent event) {
         // The light sensor returns a single value.
         // Many sensors return 3 values, one for each axis.
-        float lux = event.values[0];
+        lux = event.values[0];
 
         // Do something with this sensor value.
         Log.d(TAG, "onSensorChanged: Lux: " + lux);
 
-        Log.d(TAG, "Sensor evenType: " + event.sensor.getType());
-
-        if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-            Log.d(TAG, "onSensorChanged: X: " + event.values[0]);
-            Log.d(TAG, "onSensorChanged: Y: " + event.values[1]);
-            Log.d(TAG, "onSensorChanged: Z: " + event.values[2]);
-        }
-
+        //Log.d(TAG, "Sensor evenType: " + event.sensor.getType());
         runOnUiThread(new Runnable() {
             @Override
             public void run() {
-                if(event.sensor.getType() == Sensor.TYPE_LIGHT){
+                if(event.sensor.getType() == Sensor.TYPE_LIGHT) {
                     light.setText(Float.toString(event.values[0]));
-                }
-                if(event.sensor.getType() == Sensor.TYPE_ACCELEROMETER){
-                    x.setText(Float.toString(event.values[0]));
-                    y.setText(Float.toString(event.values[1]));
-                    z.setText(Float.toString(event.values[2]));
                 }
             }
         });
@@ -78,13 +84,40 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onResume() {
         super.onResume();
-        sensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_NORMAL);
-        sensorManager.registerListener(this, accel, SensorManager.SENSOR_DELAY_NORMAL);
+        sensorManager.registerListener(this, mLight, SensorManager.SENSOR_DELAY_GAME);
+        new Thread(new Runnable() {
+
+            @Override
+            public void run() {
+                // we add 100 new entries
+                for (int i = 0; i < 2; i++) {
+                    runOnUiThread(new Runnable() {
+
+                        @Override
+                        public void run() {
+                            addEntry();
+                        }
+                    });
+
+                    // sleep to slow down the add of entries
+                    try {
+                        Thread.sleep(600);
+                    } catch (InterruptedException e) {
+                        // manage error ...
+                    }
+                }
+            }
+        }).start();
     }
 
     @Override
     protected void onPause() {
         super.onPause();
         sensorManager.unregisterListener(this);
+    }
+
+    private void addEntry() {
+        // here, we choose to display max 10 points on the viewport and we scroll to end
+        series.appendData(new DataPoint(lastX++, lux), true, 100);
     }
 }
